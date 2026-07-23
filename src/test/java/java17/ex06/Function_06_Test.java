b7ad055deba149999ab1e8808eb2066e6d4447c3
Nicolas Bernard <njr.bernard@gmail.com>
2026-07-23 16:44:59 +0200

    TP3

--- src/test/java/java17/ex06/Function_06_Test.java
+++ src/test/java/java17/ex06/Function_06_Test.java
@@ -16,9 +16,9 @@ public class Function_06_Test {
     // tag::formatAge[]
     // TODO compléter la méthode
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
-    String formatAge(Supplier<Person> supplier) {
-        // TODO
-        return null;
+        String formatAge(Supplier<Person> supplier) {
+            Person person = supplier.get();
+        return "[age=" + person.getAge()+"]";
     }
     // end::formatAge[]
 
@@ -26,7 +26,9 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+        Person person = new Person("Nicolas", "Bernard", 35, "ABCD");
+        Supplier<Person> supplier = () -> person;
+        String result = formatAge(supplier);
 
         assert result.equals("[age=35]");
     }
--- src/test/java/java17/ex07/Function_07_Test.java
+++ src/test/java/java17/ex07/Function_07_Test.java
@@ -13,7 +13,7 @@ public class Function_07_Test {
     // TODO compléter la méthode pour qu'elle renvoie une chaîne de caractères de la forme "(<nb1><symbol><nb2>)=<resultat>"
     // TODO ex. "(10+11)=21", "(5-2)=3"
     String format(int nb1, int nb2, String symbol, IntBinaryOperator operator) {
-        // TODO
+
         return null;
     }
     // end::format[]
